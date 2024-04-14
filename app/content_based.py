ae8e037741182f88c7217892d1ce7767a0c0ddcf
sylvain gagnot <webmaster@codeheures.fr>
2024-04-14 23:28:19 +0200

    Upd: Content based

--- app/content_based.py
+++ app/content_based.py
@@ -1,21 +1,28 @@
+import numpy as np
 import pandas as pd
-import pickle
-from pandarallel import pandarallel
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
+from sklearn.metrics.pairwise import linear_kernel 
+from sklearn.decomposition import PCA
+from app.collaborative import Collaborative
 
 
 class ContentBased():
 
-    def __init__(self, artifact_path='artifacts') -> None:
+    def __init__(self, artifact_path='artifacts', top_count=5) -> None:
         self.artifacts_path = artifact_path
-        self.df_path = f'{self.artifacts_path}/df_content_based.pickle'
-        self.model_path = f'{self.artifacts_path}/model_content_based.pickle'
+        self.top_count = top_count
+        self.content_similarities = f'{self.artifacts_path}/content_similarities.npy'
 
 
-    def prepare_df(self, df_meta_data: pd.DataFrame, df_embeddings: pd.DataFrame) -> pd.DataFrame:
+    def prepare_df(self, df_meta_data: pd.DataFrame, df_embeddings: pd.DataFrame, n_components=None, sample=None, verbose=False) -> pd.DataFrame:
+        
+        # Concat meta data with embeddings
+        df_embeddings.columns = df_embeddings.columns.astype(str)
         df = pd.concat([df_meta_data, df_embeddings], axis=1)
+
+        # Sampling
+        if sample:
+            df = df.sample(sample, random_state=1234)
 
         # Scale timestanp and word_count
         scaler = StandardScaler()
@@ -24,56 +31,87 @@ class ContentBased():
         df[scaled_columns] = scaled
 
         # One hot encode categories and publisher ids
-        df = pd.get_dummies(df, columns=['category_id', 'publisher_id'])
+        df = pd.get_dummies(df, columns=['category_id', 'publisher_id'], dtype=int)
+        
+        # Apply PCA to reduce shape
+        if n_components:
+            pca = PCA(n_components=n_components)
+            df = pd.DataFrame(pca.fit_transform(df), columns=[f'PCA_{i}' for i in range(1,len(pca.explained_variance_)+1)])
+            if verbose:
+                cum_sum = np.cumsum(pca.explained_variance_ratio_)
+                pd.Series(cum_sum).plot()
+                for i in [0.8, 0.9, 0.95]:
+                    args_where = np.argwhere(np.array(cum_sum)>i)
+                    if len(args_where) > 0:
+                        print(f'{i*100}% atteint à {args_where[0][0]}')
 
         return df
     
     
-    def train(self, df: pd.DataFrame, verbose=False) -> SVD:
-
-        trainset, testset = train_test_split(df, test_size=0.3)
-        # https://heartbeat.comet.ml/recommender-systems-with-python-part-i-content-based-filtering-5df4940bd831
+    def train(self, df: pd.DataFrame, chunck_size=500, verbose=False) -> None
+        # see https://heartbeat.comet.ml/recommender-systems-with-python-part-i-content-based-filtering-5df4940bd831
+        # We must chunk train because matrix of cosine similarities may cause memory overflow up to 600Gb!
         
-        if verbose:
-            predictions = model.test(testset)
-            accuracy.rmse(predictions)
+        results = None
 
-        df.to_pickle(self.df_path)
-        pickle.dump(model, open(self.model_path, 'wb'))
-        return model
+        # Train by chunck_size lines of df to prevent MemoryError on cosine_similarities
+        for content_index in range(0,len(df),chunck_size):
+           
+            # Range to train
+            range_start = content_index
+            range_end = content_index+chunck_size
+            if verbose:
+                print(f'train from {range_start} to {range_end}', end='\r')
+            
+            current_range = df[range_start:range_end]
+
+            # Calc cosine similarities on current range
+            cosine_similarities = linear_kernel(current_range, df)
+            
+            # Sorting index of similiraties
+            cosine_index_sort = np.argsort(-cosine_similarities, axis=1)
+            
+            # keep top 6 (6 to keep 5 at end of process because self similarity will be remove later)
+            top_args = cosine_index_sort[:, 0:self.top_count+1]            
+            
+            # Remove self similiraty, and add similarity score in result
+            filtered_results = np.zeros(shape=(top_args.shape[0], top_args.shape[1]-1, 2))
+            for row_index in range(len(top_args)):
+                filtered_result = [[v, cosine_similarities[row_index, v]]  for v in top_args[row_index] if v != row_index]
+                filtered_results[row_index, 0:len(filtered_result)] = filtered_result[0:self.top_count]
+
+            if results is None:
+                results = filtered_results
+            else:
+                results = np.concatenate((results, filtered_results), axis=0)
+
+        # Saving
+        np.save(self.content_similarities, results)
+   
+        return results
     
-    
-    def predict(self, row, model):
-        return model.predict(row.uid, row.iid)
-
-
-    def get_users(self) -> list[int]:
-        df = pd.read_pickle(self.df_path)
-        return list(df.sample(10)['user_id'])
-
-
-    def user_exist(self, user_id) -> bool:
-        df = pd.read_pickle(self.df_path)
-        return len(df.loc[df['user_id'] == user_id]['user_id'].unique()) == 1
-
 
     def recommand(self, user_id:int, verbose=True) -> list[int]:
-        pandarallel.initialize(progress_bar=verbose)
 
-        df = pd.read_pickle(self.df_path)
-        model = pickle.load(open(self.model_path, 'rb'))
+        # Get user articles
+        collaborative = Collaborative()
+        user_articles = collaborative.get_user_articles(user_id=user_id)
 
-        user_articles = list(df.loc[df['user_id'] == user_id]['article_id'].unique())
-        not_viewed_article_ids = list(df.loc[~df['article_id'].isin(user_articles)]['article_id'].unique())
-        results = pd.DataFrame([[user_id, article_id, 0] for article_id in not_viewed_article_ids], columns=['uid', 'iid', 'est'])
+        # Read similarities
+        content_similarities = np.load(self.content_similarities)
 
-        results['est'] = results.parallel_apply(self.predict, args=(model,), axis=1)
+        # keep user article only if exist on content_similarities
+        filtered_user_articles = [article for article in user_articles if article < content_similarities.shape[0]]
 
-        results['iid'] = results['iid'].astype('int64')
+        # DataFrame from similarities
+        df_similarities = pd.DataFrame(content_similarities[filtered_user_articles, :].reshape(-1, 2), columns=['item_id', 'similarity'])
+        df_similarities['item_id'] = df_similarities['item_id'].astype(int)
+        
+        # Filter DataFrame to remove current user article (to not recommand already readed articles)
+        df_similarities = df_similarities[~df_similarities['item_id'].isin(user_articles)]
 
-        results.sort_values(by='est', ascending=False, inplace=True, ignore_index=True)
-
-        top_recommand = list(results.loc[0:5, 'iid'])
+        # Sort top 5 articles by similarity score
+        top_recommand = df_similarities.sort_values(by='similarity', ascending=False)[0:content_similarities.shape[1]]['item_id'].to_list()
         
         if verbose:
             print(f"Current user articles: {', '.join(map(str, user_articles))}")
--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 import logging
 from app.collaborative import Collaborative
+from app.content_based import ContentBased
 from app.data import Data
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -33,16 +34,43 @@ def recommand(req: func.HttpRequest) -> func.HttpResponse:
     
 @app.route(route="train/collaborative", methods=["GET"])
 def train_collaborative(req: func.HttpRequest) -> func.HttpResponse:
+    
     logging.info(f'Entrainement modèle collaboratif demandé')
     data = Data()
     collaborative = Collaborative()
     sample_users_size = 20000
+    
     logging.info(f'Lecture des fichiers clicks en cours...')
     df_clicks = data.read_clicks()
     sample_df = collaborative.prepare_df(df_clicks=df_clicks, max_clicks=5, n_users=sample_users_size)
+    
     logging.info(f'Entrainement en cours...')
-    collaborative.train(sample_df, verbose=False)
+    model = collaborative.train(sample_df, verbose=False)
+    
     return func.HttpResponse(
              "Entrainement du model collaborative terminé",
+             status_code=200
+        )
+
+@app.route(route="train/content-based", methods=["GET"])
+def train_collaborative(req: func.HttpRequest) -> func.HttpResponse:
+    
+    logging.info(f'Entrainement modèle content based demandé')
+    data = Data()
+    content_based = ContentBased()
+    sample = 20000
+    
+    logging.info(f'Lecture des fichiers meta data et embeddings en cours...')
+    df_meta_data = data.read_articles_meta_data()
+    df_embeddings = data.read_embeddings()
+
+    logging.info(f'Preparation du dataframe...')
+    df = content_based.prepare_df(df_meta_data=df_meta_data, df_embeddings=df_embeddings, n_components=43, sample=sample)
+    
+    logging.info(f'Entrainement en cours...')
+    results = content_based.train(df=df, chunck_size=500)
+    
+    return func.HttpResponse(
+             "Entrainement du model content based terminé",
              status_code=200
         )
